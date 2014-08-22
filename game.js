83e073638e12f2b0ec48a30e81f7fdd942ec46ee
Richard Harrington <rwharrington87@gmail.com>
2014-08-22 08:26:03 -0400

    add confines to the edge of the map

--- game.js
+++ game.js
@@ -60,17 +60,30 @@
   var update = function(modifier) {
 
     if (38 in keysDown) {//up
+      if (hero.y < 10) {
+        return;
+      }
       hero.y -= hero.speed * modifier;
     }
     if (40 in keysDown) {//down
+      if (hero.y > canvas.height - 40) {
+        return;
+      }
       hero.y += hero.speed * modifier;
     }
     if (37 in keysDown) {//left
+      if (hero.x < 10) {
+        return;
+      }
       hero.x -= hero.speed * modifier;
     }
     if (39 in keysDown) {//right
+      if (hero.x > canvas.width - 40) {
+        return;
+      }
       hero.x += hero.speed * modifier;
     }
+
 
     // Are they touching?
     if (hero.x <= (monster.x + 32)
